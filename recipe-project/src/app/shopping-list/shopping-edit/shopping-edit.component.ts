3e62a20705b2057feb1ecfb44d04a3a4269daff2
karanbhalla <kbhalla22@gmail.com>
2020-06-13 13:45:57 -0400

    initialising the backend for ecommerce project

--- recipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter } from '@angular/core';
+import { string } from 'prop-types';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+@ViewChild ('nameInput',{static:false})nameInputRef :  ElementRef;
+@ViewChild ('amountInput',{static:false})amountInputRef :  ElementRef;
+ingredientAdded=new EventEmitter<{name:string,amount:number}>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+onAddItem(){
+  const 
+const newIngredient =new Ingredient(this.nameInputRef.nativeElement.value);
+}
 }
