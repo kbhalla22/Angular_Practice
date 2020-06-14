eebcfc302d4133f4248300c686f2f8ee65968335
karanbhalla <kbhalla22@gmail.com>
2020-06-13 20:12:12 -0400

    MArked the class as a configuration class

--- EcommerceProject/02-backend/springboot-ecommerce/src/main/java/com/karanbhalla/ecommerce/config/MyDataRestConfig.java
+++ EcommerceProject/02-backend/springboot-ecommerce/src/main/java/com/karanbhalla/ecommerce/config/MyDataRestConfig.java
@@ -1,10 +1,13 @@
 package com.karanbhalla.ecommerce.config;
 
 import com.karanbhalla.ecommerce.entity.Product;
+import com.karanbhalla.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -12,6 +15,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         //disable http methods for products
         config.getExposureConfiguration().forDomainType(Product.class).withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration().forDomainType(ProductCategory.class).withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
 
     }
