b0df767b4c986d15e2112a228a3bda46e6d41490
karanbhalla <kbhalla22@gmail.com>
2020-06-20 12:44:43 -0400

    completed the search functionality

--- EcommerceProject/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ EcommerceProject/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 products: Product[];
 currentCategoryId: number;
 currentCategoryName: string;
+searchMode : boolean;
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -22,23 +23,42 @@ currentCategoryName: string;
   }
 
   listProducts(){
-
-    //check if if paramter is vailable
-    const hasCategoryId: boolean=this.route.snapshot.paramMap.has('id');
-
-    if(hasCategoryId){
-      this.currentCategoryId=+this.route.snapshot.paramMap.get('id');
-      this.currentCategoryName=this.route.snapshot.paramMap.get('name');
-    } else{
-      this.currentCategoryId=1;
-      this.currentCategoryName='Books';
+    this.searchMode=this.route.snapshot.paramMap.has('keyword');
+    if(this.searchMode){
+      this.handleListProducts()
+    }
+    else{
+      this.handleListProducts();
     }
 
-    //get products for given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+    
+  }
+
+  handleSearchProducts(){
+    const theKeyword: string =this.route.snapshot.paramMap.get('keyword');
+    this.productService.searchProducts(theKeyword).subscribe(
       data=>{
         this.products=data;
       }
     )
+  }
+  handleListProducts(){
+//check if if paramter is vailable
+const hasCategoryId: boolean=this.route.snapshot.paramMap.has('id');
+
+if(hasCategoryId){
+  this.currentCategoryId=+this.route.snapshot.paramMap.get('id');
+  this.currentCategoryName=this.route.snapshot.paramMap.get('name');
+} else{
+  this.currentCategoryId=1;
+  this.currentCategoryName='Books';
+}
+
+//get products for given category id
+this.productService.getProductList(this.currentCategoryId).subscribe(
+  data=>{
+    this.products=data;
+  }
+)
   }
 }
