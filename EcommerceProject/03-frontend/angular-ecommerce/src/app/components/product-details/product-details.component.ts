acb00de321651af7d4d8ccd2e8f27c4071605a23
karanbhalla <kbhalla22@gmail.com>
2020-06-20 17:40:51 -0400

    fixed missing entity errors

--- EcommerceProject/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ EcommerceProject/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-product:Product;
+product:Product =new Product();
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
 
