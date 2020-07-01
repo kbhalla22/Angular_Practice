01266defb3f1c243c762cb89a03baf8166c6ce8f
karanbhalla <kbhalla22@gmail.com>
2020-07-01 14:36:16 -0400

    added  a directive opposite to ngif

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,24 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, Input } from '@angular/core';
+import { NgModuleResolver } from '@angular/compiler';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+@Input() defaultColor: string ='transparent';
+@Input() highlightColor: string='blue';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event){
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+  @HostListener('mouseleave') mouseLeave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
---
+++ directives-start/src/app/unless.directive.ts
@@ -0,0 +1,15 @@
+import { Input, Directive, TemplateRef, ViewContainerRef } from "@angular/core";
+
+@Directive({
+    selector:'[appUnless]'
+})
+export class UnlessDirective{
+    @Input() set unless(condition:boolean){
+if(!condition){
+this.vcRef.createEmbeddedView(this.templateRef);
+} else{
+this.vcRef.clear();
+}
+    }
+    constructor(private templateRef: TemplateRef<any>,private vcRef: ViewContainerRef){}
+}
