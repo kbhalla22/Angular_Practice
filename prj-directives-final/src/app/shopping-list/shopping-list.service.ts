29bda9ac3690b5b8ccea444fe128e52fc5d9c1c4
karanbhalla <kbhalla22@gmail.com>
2020-07-02 16:04:46 -0400

    made services more modular

--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,12 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService{
-    
+   private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+
+      getRecipes(){
+        return this.ingredients.slice();
+    }
 }
