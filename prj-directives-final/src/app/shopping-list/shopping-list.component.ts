970849f833c2044f2ad897ace22c97493df7a314
karanbhalla <kbhalla22@gmail.com>
2020-07-02 21:17:34 -0400

    updated services

--- prj-directives-final/src/app/shopping-list/shopping-list.component.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.component.ts
@@ -14,7 +14,7 @@ export class ShoppingListComponent implements OnInit {
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
-    this.ingredients=this.shoppingListService.getRecipes();
+    this.ingredients=this.shoppingListService.getIngredients();
     this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[])=>{
         this.ingredients=ingredients;
--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -8,12 +8,18 @@ export class ShoppingListService {
     new Ingredient("Tomatoes", 10)
   ];
 
-  getRecipes() {
+  getIngredients() {
     return this.ingredients.slice();
   }
 
   addIngredient(ingredient: Ingredient){
       this.ingredients.push(ingredient);
       this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients : Ingredient[]){
+      this.ingredients.push(...ingredients);
+      this.ingredientsChanged.emit(this.ingredients.slice());
+
   }
 }
