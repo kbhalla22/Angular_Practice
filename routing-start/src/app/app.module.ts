86204a09d6ca28b07b6e7fb9ff95323da85e2d81
karanbhalla <kbhalla22@gmail.com>
2020-07-04 16:46:32 -0400

    trigger the routes programmatically.

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -31,7 +31,7 @@ const appRoutes: Routes =[
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
